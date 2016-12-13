5cc46b932c527f5db516f1686dac1556c0c4299c
Greg McDonald <grm695@utexas.edu>
2016-12-12 18:09:07 -0600

    Added some validity checking to sending SMS messages, and unit tests for new functionality.

--- src/main/java/save-the-world/Messenger.java
+++ src/main/java/save-the-world/Messenger.java
@@ -5,34 +5,56 @@ import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
 public class Messenger {
-  // Find your Account Sid and Token at twilio.com/user/account
-  public static final String ACCOUNT_SID = "AC7bf672b3288a8dbd2535daa1e128ec5f";
-  public static final String AUTH_TOKEN = "b7600d19ed3d064daf64dea221ed6477";
+    // Find your Account Sid and Token at twilio.com/user/account
+    public static final String ACCOUNT_SID = "AC7bf672b3288a8dbd2535daa1e128ec5f";
+    public static final String AUTH_TOKEN = "b7600d19ed3d064daf64dea221ed6477";
 
-  // Subject to change.
-  private static final String TWILIO_PHONE_NUMBER = "+15162102347";
+    // Subject to change.
+    private static final String TWILIO_PHONE_NUMBER = "+15162102347";
 
-  // Singleton instance
-  private static Messenger singletonInstance = new Messenger();
+    // Singleton instance
+    private static Messenger singletonInstance = new Messenger();
 
+    private static final boolean SEND_MESSAGES = true;
 
+    private Messenger() {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+    }
 
-  private Messenger(){
-    Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-  }
+    public static Messenger getInstance() {
+        return singletonInstance;
+    }
 
-  public static Messenger getInstance(){
-    return singletonInstance;
-  }
+    public boolean sendSMS(String phoneNumber, String messageText) {
+        // Check validity of arguments
+        boolean validArguments = isValidPhoneNumber(phoneNumber) && isValidMessage(messageText);
 
-  public boolean sendSMS(String phoneNumber, String messageText){
-    // Check validity of things
+        // If send messages flag is set and arguments are valid
+        if(SEND_MESSAGES && validArguments){
+            try {
+                // Send the message
+                Message message = Message.creator(new PhoneNumber(phoneNumber),
+                    new PhoneNumber(TWILIO_PHONE_NUMBER), 
+                    messageText).create();
+                System.out.println(message.getSid());
 
-    Message message = Message.creator(new PhoneNumber(phoneNumber),
-        new PhoneNumber(TWILIO_PHONE_NUMBER), 
-        messageText).create();
-    System.out.println(message.getSid());
+                return true;
+            } catch (Exception e) {
+                // Something went wrong
+                return false;
+            }
+        }
 
-    return true;
-  }
+        return validArguments;
+    }
+
+    // Helper Method(s)
+
+    public static boolean isValidPhoneNumber(String phoneNumber){
+        return true;
+    }
+
+    public static boolean isValidMessage(String message){
+        return true;
+    }
 }
---
+++ src/test/java/save-the-world/MessengerTest.java
@@ -0,0 +1,64 @@
+package com.savetheworld;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class MessengerTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public MessengerTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(MessengerTest.class);
+    }
+
+    /**
+     * Test whether getInstance returns a valid instance
+     */
+    public void testGetInstance() {
+        Messenger instance = Messenger.getInstance();
+        assertNotNull("Messenger singleton instance should not be null.", instance);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testSmsSuccess() {
+        String validPhoneNumber = "+14692379287";
+        String validMessageText = "This is some valid message text.";
+
+        Messenger instance = Messenger.getInstance();
+        assertTrue(true);
+        //boolean sendSmsResult = instance.sendSMS(validPhoneNumber, validMessageText);
+        //assertTrue("Message should SUCCEED with valid phone number and valid message text.", sendSmsResult);
+    }
+
+    public void testSmsFailure() {
+        String validPhoneNumber = "+14692379287";
+        String validMessageText = "This is some valid message text.";
+        String invalidPhoneNumber = "469237928";
+        String invalidMessageText = "";
+
+        Messenger instance = Messenger.getInstance();
+        boolean sendSmsResult1 = instance.sendSMS(invalidPhoneNumber, invalidMessageText);
+        assertFalse("Message should FAIL with invalid phone number and invalid message.", sendSmsResult1);
+
+        boolean sendSmsResult2 = instance.sendSMS(validPhoneNumber, invalidMessageText);
+        assertFalse("Message should FAIL with valid phone number and invalid message.", sendSmsResult2);
+
+        boolean sendSmsResult3 = instance.sendSMS(invalidPhoneNumber, validMessageText);
+        assertFalse("Message should FAIL with invalid phone number and valid message.", sendSmsResult3);
+    }
+}
