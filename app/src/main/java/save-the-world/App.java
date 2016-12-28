9f959c193832d9d6d074627ba382e94ea57e1147
Greg McDonald <grm695@utexas.edu>
2016-12-27 22:38:06 -0600

    Removed use of deprecated Firebase API, now use HTTPS requests for Firebase.

--- app/src/main/java/save-the-world/App.java
+++ app/src/main/java/save-the-world/App.java
@@ -4,18 +4,23 @@ import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
+import org.json.simple.JSONObject;
+
 import java.util.ArrayList;
 
 public class App {
-    // Developer phone number(s), for testing. XXX REMOVE LATER
+    // The phone number from which messages are sent.
+    private static final String TWILIO_PHONE_NUMBER = "+15162102347";
+
+    // XXX: Developer phone number(s), for testing. Remove once development is complete.
     private static final String ADAM_PHONE_NUMBER = "+15163533154";
     private static final String GREG_PHONE_NUMBER = "+14692379287";
     private static final String KIERAN_PHONE_NUMBER = "+17136208645";
 
     public static void main(String[] args) {
         // Test Firebase
-        Database.getInstance().saveConversation("The eagle has landed.");
-        Database.getInstance().disconnect();
+        JSONObject result = Database.getInstance().getConversation(TWILIO_PHONE_NUMBER, GREG_PHONE_NUMBER);
+        System.out.println(result);
 
         // Test messaging
         //  Messenger messenger = Messenger.getInstance();
--- app/src/main/java/save-the-world/Database.java
+++ app/src/main/java/save-the-world/Database.java
@@ -3,12 +3,60 @@ package com.savetheworld;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 
-import java.util.concurrent.atomic.AtomicBoolean;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
 
 public class Database {
   
     // Singleton instance
     private static Database instance = new Database();
+
+    private Database(){
+
+    }
+
+    public static Database getInstance(){
+        return instance;
+    }
+
+    public JSONObject getConversation(String participantPhoneNumber, String otherParticipantPhoneNumber){
+        String testUrl = "https://save-the-world-c2795.firebaseio.com/conversations.json";
+        try{
+            URL url = new URL(testUrl);
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+
+            int responseCode = connection.getResponseCode();
+            switch (responseCode) {
+                case 200:
+                case 201:
+                    BufferedReader connectionStreamReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String connectionResult = "";
+                    String line;
+                    while ((line = connectionStreamReader.readLine()) != null) {
+                        connectionResult += line;
+                    }
+                    connectionStreamReader.close();
+                    return (JSONObject) (new JSONParser()).parse(connectionResult);
+                default:
+                    System.err.println("Connection failed with response code " + responseCode + ".");
+            }
+        } catch (Exception e) {
+            // XXX: Remove printing
+            System.err.println("Error occurred while get conversation over HTTPS.");
+        }
+        return null;
+    }
+
+    /* // DEPRECATED BELOW, uses deprecated API
+    import java.util.concurrent.atomic.AtomicBoolean;
 
     // Firebase
     private static final String FIREBASE_URL = "https://save-the-world-c2795.firebaseio.com/";
@@ -17,10 +65,6 @@ public class Database {
     private Database(){
         // Database is created in a connected state
         firebaseReference = new Firebase(FIREBASE_URL);
-    }
-
-    public static Database getInstance(){
-        return instance;
     }
 
     public void saveConversation(String message){
@@ -44,4 +88,5 @@ public class Database {
     public void disconnect(){
         firebaseReference.goOffline();
     }
+    */
 }
