127ba330cad0f0e42d9a86547e4b9ce3547a711c
Greg McDonald <grm695@utexas.edu>
2016-12-28 12:12:43 -0600

    Implemented Comparable interface.

--- app/src/main/java/save-the-world/MessageRecord.java
+++ app/src/main/java/save-the-world/MessageRecord.java
@@ -2,7 +2,10 @@ package com.savetheworld;
 
 import java.util.Date;
 
-public class MessageRecord {
+import java.lang.NullPointerException;
+import java.lang.ClassCastException;
+
+public class MessageRecord implements Comparable<MessageRecord> {
 
     public String to;
     public String from;
@@ -19,5 +22,22 @@ public class MessageRecord {
         this.from = from;
         this.body = body;
         this.timestamp = timestamp;
+    }
+
+    public MessageRecord(MessageRecord messageRecord){
+        this.to = messageRecord.to;
+        this.from = messageRecord.from;
+        this.body = messageRecord.body;
+        this.timestamp = messageRecord.timestamp;
+    }
+
+    public int compareTo(MessageRecord messageRecord){
+        if(messageRecord == null){
+            throw new NullPointerException("Specified object is null in comparison of MessageRecord(s).");
+        } else if (!(messageRecord instanceof MessageRecord)) {
+            throw new ClassCastException("Specified object's type prevents it from being compared to a MessageRecord.");
+        }
+
+        return this.timestamp.compareTo(messageRecord.timestamp);
     }
 }
